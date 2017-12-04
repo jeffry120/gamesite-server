d4bb13b72e7c4fd1a88bbf6b99f77f948026e1a4
unknown <jeffry-1997@outlook.com>
2017-12-04 15:18:21 +0100

    update

--- model/game.model.js
+++ model/game.model.js
@@ -12,6 +12,10 @@ const GameSchema = new Schema({
         type: String,
         required: true
     },
+    genre: {
+        type: String,
+        required: true
+    },
     imagePath: String,
     characters: [GamecharacterSchema],
     creators: []
@@ -34,6 +38,7 @@ Game.count({}, function (err, count) {
             'Overwatch ended the crisis, and helped maintain peace in the decades that followed, inspiring an era of exploration, innovation, and discovery. But, after many years, Overwatch’s influence waned, and it was eventually disbanded.\n' +
             '\n' +
             'Now, conflict is rising across the world again, and the call has gone out to heroes old and new. Are you with us?',
+            genre: 'First person shooter',
             imagePath: 'https://static-cdn.jtvnw.net/ttv-boxart/Overwatch.jpg',
             characters: [
                 {
@@ -62,6 +67,7 @@ Game.count({}, function (err, count) {
             name: 'Final Fantasy',
             description: 'Enter an era of war within the world of Ivalice. The small kingdom of Dalmasca, conquered by the Archadian Empire, is left in ruin and uncertainty. Princess Ashe, the one and only heir to the throne, devotes herself to the resistance to liberate her country. Vaan, a young man who lost his family in the war, dreams of flying freely in the skies. In a fight for freedom and fallen royalty, join these unlikely allies and their companions as they embark on a heroic adventure to free their homeland. ',
             imagePath: 'http://image.gamersky.com/zqimg/ff12/image/cover1_b.jpg',
+            genre: 'JRPG',
             characters: [
                 {
                     name: 'Balthier',
