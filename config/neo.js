3b1ff472b5963d263643060e81c16bcfacbd2958
unknown <jeffry-1997@outlook.com>
2017-12-13 18:55:21 +0100

    heroku

--- config/neo.js
+++ config/neo.js
@@ -4,7 +4,7 @@ const neo4j = require('neo4j-driver').v1;
 //var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "123456"));
 
 if(process.env.NODE_ENV ===  'production'){
-    var driver = neo4j.driver("bolt://hobby-gojbbepjgmiigbkediphfial.dbs.graphenedb.com:24786", neo4j.auth.basic("serie-graph", "b.piWUERr38SEs.xzDcne55KbpGRA4l"));
+    var driver = neo4j.driver("bolt://hobby-mkdmfblfobflgbkenjomhjal.dbs.graphenedb.com:24786", neo4j.auth.basic("gameserverbyjeffry", "b.epnlZTmiay9B.obeT0zvOqBdSQ6ke"));
 }else {
     var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("", ""));
 }
